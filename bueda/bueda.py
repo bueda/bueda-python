2912f0cdfd5bff5ccd5e1d5fb4cf490ebb5916cb
Christopher Peplin <peplin@bueda.com>
2010-05-22 19:17:55 -0400

    Change concept_id -> uri. Patch version bump.

--- bueda/bueda.py
+++ bueda/bueda.py
@@ -59,7 +59,7 @@ class Semantic(object):
       original : The list of inputs that led to this result
     '''
     def __init__(self, types, concept_id, original):
-        self.types = types
+        self.types = [typ['name'] for typ in types]
         self.concept_id = concept_id
         self.original = original
     def __str__(self):
@@ -177,6 +177,6 @@ def enrich(tags, api_key=None):
             split=result['split'],
             canonical=result['canonical'],
             categories=[Category(c['name'], c['confidence']) for c in result['categories']],
-            semantic=[Semantic(s['types'], s['concept_id'], s['original']) for s in result['semantic']],
+            semantic=[Semantic(s['types'], s['uri'], s['original']) for s in result['semantic']],
             )
 
