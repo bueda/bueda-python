a497ddbf4978cd3183514fde83735d2773f63dca
Molly Samuels <mollysamuels@molly-samuelss-macbook.local>
2010-05-28 16:35:27 -0400

    Added name attribute to semantic in enrich method

--- bueda/bueda.py
+++ bueda/bueda.py
@@ -186,7 +186,7 @@ def enrich(tags, api_key=None):
             split=result['split'],
             canonical=result['canonical'],
             categories=[Category(c['name'], c['confidence']) for c in result['categories']],
-            semantic=[Semantic(s['types'], s['uri'], s['original']) for s in result['semantic']],
+            semantic=[Semantic(s['name'], s['types'], s['uri'], s['original']) for s in result['semantic']],
             )
 
 
